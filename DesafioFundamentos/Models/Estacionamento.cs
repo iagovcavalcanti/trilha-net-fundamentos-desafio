0278c278e1768c5fa53da9beb455bcbc48be5533
iagovcavalcanti <iago.vcavalcanti@gmail.com>
2023-11-15 21:26:01 -0300

    Entrega do sistema console estacionamento do bootcamp avanade

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
@@ -14,38 +16,75 @@ namespace DesafioFundamentos.Models
 
         public void AdicionarVeiculo()
         {
-            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*
+            string veiculo = string.Empty;
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            veiculo = Console.ReadLine();
+
+            veiculo = veiculo.Replace(" ", "");
+            if (string.IsNullOrEmpty(veiculo))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Por favor digite algo");
+                Console.ResetColor();
+            }
+            else if (ValidarPlaca(veiculo) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Por favor digitar uma placa válida no formato XXX-1234 \nApenas letras maúsculas e números");
+                Console.ResetColor();
+            }
+            else if (veiculos.Any(x => x == veiculo))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Veículo já cadastrado.");
+                Console.ResetColor();
+            }
+            else
+            {
+                veiculos.Add(veiculo);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Veículo adicionado com sucesso!");
+                Console.ResetColor();
+            }
         }
 
+        static bool ValidarPlaca(string veiculo)
+        {
+            string validacaoPlaca = @"[A-Z]{3}[-][0-9]{4}";
+            Regex regex = new Regex(validacaoPlaca);
+            return regex.IsMatch(veiculo);
+        }
         public void RemoverVeiculo()
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
+            string placa = Console.ReadLine();
 
-            // Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
-            string placa = "";
 
-            // Verifica se o veículo existe
             if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
-                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
-                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                string horas = string.Empty;
+                decimal valorTotal = 0;
+                horas = Console.ReadLine();
+                if (Convert.ToInt32(horas) > 0)
+                {
+                    valorTotal = precoInicial + (Convert.ToInt32(horas) * precoPorHora);
+                }
+                bool removido = veiculos.Remove(placa);
+                if (removido == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                    Console.ResetColor();
+                }
 
-                // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
-
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.ResetColor();
             }
         }
 
@@ -55,12 +94,17 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
-                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                for (int i = 0; i < veiculos.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {veiculos[i]}");
+                }
+
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Não há veículos estacionados.");
+                Console.ResetColor();
             }
         }
     }
